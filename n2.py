2d4fda7958ec9c8ede8f63ea4be501204f5c63d1
NeKordebalet <nekordebalet2@tutamail.com>
2025-05-31 18:46:42 +0300

    nbvcxz

--- n2.py
+++ n2.py
@@ -1,25 +1,28 @@
 import random
 import string
 from multiprocessing import Process, Lock
+
 def random_string(length):
     return ''.join(random.choices(string.ascii_letters + string.digits, k=length))
+
 def write_to_file(lock, filename, num_chars):
     random_str = random_string(num_chars)
-    with lock: 
+    with lock:
         with open(filename, 'a') as f:
             f.write(random_str + '\n')
         print(f"Записано: {random_str}")
-def main():
+
+def multiprocessing():
     num_processes = 10
     num_chars = 10
     filename = 'output.txt'
-    lock = Lock()  
+    lock = Lock()
     processes = []
+    
     for _ in range(num_processes):
         p = Process(target=write_to_file, args=(lock, filename, num_chars))
         p.start()
         processes.append(p)
+    
     for p in processes:
         p.join()
-if __name__ == '__main__':
-    main()
